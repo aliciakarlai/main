b288770cf7aa588d62b2e10c5b34ee9a762a8bed
aliciakarlai <156141913+aliciakarlai@users.noreply.github.com>
2024-07-18 16:38:51 -0600

    Update nav.py

--- nav.py
+++ nav.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 pages = st.navigation([
     st.Page("home.py", title="Home"),
-    st.Page("SimpleGBV.py", title="GBV with Background Colors"),
+    st.Page("simpleGBV.py", title="GBV with Background Colors"),
     st.Page("measurementGBV.py", title="GBV With Measurements"),
     st.Page("Instructions.py", title="Instructions"),  
 ])
