77c124844d55ec77d90f6aeec08e423c4b3cc4c9
aliciakarlai <156141913+aliciakarlai@users.noreply.github.com>
2024-07-18 16:14:45 -0600

    Create home.py

---
+++ home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Gun Barrel View Generator")
+st.write("Create 2 different types of GBV's in minutes")
+
+st.header("GBV With Background Colors:rainbow:")
+
+#get image url
+Measured_url="https://github.com/aliciakarlai/streamlit/raw/main/GBVcolors.jpg"
+
+# Display the image
+st.image(Measured_url, width=450)
+
+#get image url
+st.header("GBV With Measurements:straight_ruler:")
+
+# Display the image
+Color_url="https://github.com/aliciakarlai/streamlit/raw/main/GBVmeasured.jpg"
+st.image(Color_url, width=450)
